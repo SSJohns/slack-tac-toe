94ee2431061041c0d6959ddc6e08e8145185e384
Shaquille Johnson <sjohns25@nd.edu>
2016-11-16 15:14:10 -0500

    game end board printing

--- channels.py
+++ channels.py
@@ -39,7 +39,7 @@ class Channel:
             }
         )
         # import ipdb; ipdb.set_trace()
-        return self.game.print_game() + '\n' + "Created a new game state, it is {!s}\'s turn. To make move `/ttt move [1-9]`.".format(self.turn['name'])
+        return self.game.print_game() + '\n' + "Created a new game state, it is @{!s}\'s turn. To make move `/ttt move [1-9]`.".format(self.turn['name'])
 
     def move_piece(self, player, spot):
         print player, self.active, self.turn
@@ -49,7 +49,7 @@ class Channel:
             if player['id'] == self.challenger['id']:
                 if self.game.move(player, spot):
                     self.turn = self.opponent
-                    self.active['state'] = 'It is {!s}\'s turn. They are {!s}'.format(self.opponent['name'], self.game.opponent_sym)
+                    self.active['state'] = 'It is @{!s}\'s turn. They are {!s}'.format(self.opponent['name'], self.game.opponent_sym)
                     if self.game.winning_move():
                         self.active['status'] = 'Done'
                         self.active['state'] = 'Game Ended winner is {!s}'.format(self.game.winner['name'])
@@ -59,7 +59,7 @@ class Channel:
             else:
                 if self.game.move(player, spot):
                     self.turn = self.challenger
-                    self.active['state'] = 'It is {!s}\'s turn. They are {!s}'.format(self.challenger['name'], self.game.challenger_sym)
+                    self.active['state'] = 'It is @{!s}\'s turn. They are {!s}'.format(self.challenger['name'], self.game.challenger_sym)
                     if self.game.winning_move():
                         self.active['status'] = 'Done'
                         self.active['state'] = 'Game Ended winner is {!s}'.format(self.game.winner['name'])
@@ -85,14 +85,14 @@ class Channel:
             self.winner = self.opponent['name']
             self.active['status'] = 'Done'
             self.active['winner'] = self.opponent['name']
-            board = self.game.print_board()
+            board = self.game.print_game()
             self.game.reset_game()
             return self.game.print_game() + '\n' + '{!s}\n{!s} forfeited the game, {!s} is the winner'.format(board, user['name'], self.opponent['name'])
         elif user['id'] == self.opponent['id']:
             self.winner = self.challenger
             self.active['status'] = 'Done'
             self.active['winner'] = self.challenger
-            board = self.game.print_board()
+            board = self.game.print_game()
             self.game.reset_game()
             return self.game.print_game() + '\n' + '{!s}\n{!s} forfeited the game, {!s} is the winner'.format(board, user['name'], self.challenger['name'])
         else:
